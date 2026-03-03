505bcd7bb8e87caef00d260a0e6b0a96544903c9
mateuszral <mral16102000@gmail.com>
2026-03-03 21:27:33 +0100

    migrate sqlite -> postgreSQL and add column joined_at

--- app/config.py
+++ app/config.py
@@ -10,5 +10,5 @@ class Config:
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
     PERMANENT_SESSION_LIFETIME = timedelta(days=30)
     UPLOAD_FOLDER = 'static/uploads'
-    SQLALCHEMY_DATABASE_URI = os.getenv("FLASK_DB_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("POSTGRESQL_DB_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/models/user.py
+++ app/models/user.py
@@ -6,7 +6,7 @@ from app import db
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'user'
+    __tablename__ = 'app_user'
     
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -16,13 +16,18 @@ class User(db.Model, UserMixin):
     change_password = db.Column(db.Boolean, default=True)
     featured = db.Column(db.Boolean, default=False)
     avatar = db.Column(db.String(255), nullable=True)
+    joined_at = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
     
-    user_info = db.relationship('UserInfo', backref='user', uselist=False, cascade='all, delete-orphan')
+    user_info = db.relationship('UserInfo', back_populates='user', uselist=False, cascade='all, delete-orphan')
     
 class UserInfo(db.Model):
+    __tablename__ = 'user_info'
+    
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(), db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('app_user.id', ondelete='CASCADE'), primary_key=True)
     first_name = db.Column(db.String(80), nullable=True)
     last_name = db.Column(db.String(80), nullable=True)
     age = db.Column(db.Integer, nullable=True)
     bio = db.Column(db.Text, nullable=True)
+    
+    user = db.relationship('User', back_populates='user_info')
